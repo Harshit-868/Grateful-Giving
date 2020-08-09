384f0c49cc8f4e51473e153032188b5244b785ff
Harshit-868 <61592605+Harshit-868@users.noreply.github.com>
2020-08-09 19:19:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,11 @@ var int = setInterval(showSlides, 4500);
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+  if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
